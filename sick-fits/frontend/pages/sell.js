cb16ee667b3f839da8a527389f755c7aed9f36bf
Stefan <stefan1234@gmail.com>
2018-10-23 19:53:24 -0300

    asdf

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Sell = () => {
+  return (
+    <div>
+       <p>Hey! 2</p>
+      <Link href="/"><a>Home</a></Link>
+    </div>
+  )
+}
+
+export default Sell
